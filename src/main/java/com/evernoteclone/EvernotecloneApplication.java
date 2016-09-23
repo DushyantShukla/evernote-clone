304a3f300b10bc6db31c34b38dd26b52dea8243d
DushyantShukla <dushyantshukla.93@gmail.com>
2016-09-23 17:43:23 +0530

    committing the project directory structure

---
+++ src/main/java/com/evernoteclone/EvernotecloneApplication.java
@@ -0,0 +1,12 @@
+package com.evernoteclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvernotecloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvernotecloneApplication.class, args);
+	}
+}
